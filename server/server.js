3b973944b0eec17e8fa9ced1579162596331ccc0
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-10-25 09:41:43 -0500

    emmit and on

--- server/server.js
+++ server/server.js
@@ -1,20 +1,42 @@
 const express = require('express');
-
+const socketIO = require('socket.io')
+const http = require('http')
 const path = require('path');
 
 const app = express();
-
+let server = http.createServer(app);
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+let io = socketIO(server)
+io.on('connection', (client) => {
+    console.log('connected user');
+
+    client.emit('enviarmensaje', {
+        user : 'admin',
+        message : 'welcome'
+    })
+    client.on('disconnect',() => {
+        console.log('user disconnected');
+    })
+
+    // listen 
+    client.on('enviarmensaje',(message) => {
+        console.log(message);
+        
+    })
+    
+});
 
 
-app.listen(port, (err) => {
+
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
-    console.log(`Servidor corriendo en puerto ${ port }`);
+    console.log(`Server on :  ${ port }`);
+    
 
 });
